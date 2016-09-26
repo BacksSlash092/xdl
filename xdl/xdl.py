f6c0761a25c840d3a1cf27199a0bc30a547cf611
Greg Borelly <gregborrelly@gmail.com>
2016-09-26 14:55:09 -0400

    Update xdl.py

--- xdl/xdl.py
+++ xdl/xdl.py
@@ -1,6 +1,6 @@
 """
-This script takes a list of youtube URLS and proceds to download them 
-It either takes or creates a directory to place the files in them.
+This script takes a list of Youtube URLS and proceeds to download the videos 
+It finds or creates a directory to place the files in them.
 
 This script is more of an add-on to youtube-dl.
 
